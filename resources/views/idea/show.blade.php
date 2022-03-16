d39c398db5f0a3bc4610d1a2c369a13d3118e129
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2022-03-16 13:44:24 +0100

    testing showing ideas

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -17,7 +17,7 @@
             </div>
             <div class="w-full mx-2 md:mx-4">
                 <h4 class="text-xl font-semibold">
-                    <a href="#" class="hover:underline"></a>
+                    <a href="#" class="hover:underline">{{ $idea->title }}</a>
                 </h4>
                 <div class="text-gray-600 mt-3">
                     {{$idea->description}}
