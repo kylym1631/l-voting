a0d19e21da857a39c991371e832a659ab570c017
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2022-03-28 15:23:37 +0200

    set idea status

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,16 +6,15 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
         'name',
@@ -24,9 +23,9 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes that should be hidden for arrays.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
@@ -34,9 +33,9 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
+     * The attributes that should be cast to native types.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
@@ -56,11 +55,9 @@ class User extends Authenticatable
     {
         $firstCharacter = $this->email[0];
 
-        if (is_numeric($firstCharacter)) {
-            $integerToUse = ord(strtolower($firstCharacter)) - 21;
-        } else {
-            $integerToUse = ord(strtolower($firstCharacter)) - 96;
-        }
+        $integerToUse = is_numeric($firstCharacter)
+            ? ord(strtolower($firstCharacter)) - 21
+            : ord(strtolower($firstCharacter)) - 96;
 
         return 'https://www.gravatar.com/avatar/'
             .md5($this->email)
@@ -70,4 +67,11 @@ class User extends Authenticatable
             .'.png';
     }
 
+    public function isAdmin()
+    {
+        return in_array($this->email, [
+            'kylym1631@gmail.com',
+
+        ]);
+    }
 }
