9160cba73c2c7c04bc2f7d9220e8f210e8891ba1
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2022-04-07 02:02:05 +0200

    small fixes

--- app/Http/Livewire/DeleteIdea.php
+++ app/Http/Livewire/DeleteIdea.php
@@ -23,9 +23,6 @@ class DeleteIdea extends Component
             abort(Response::HTTP_FORBIDDEN);
         }
 
-        Vote::where('idea_id', $this->idea->id)->delete();
-
-        Comment::where('idea_id', $this->idea->id)->delete();
 
         Idea::destroy($this->idea->id);
 
--- database/migrations/2022_03_17_194725_create_votes_table.php
+++ database/migrations/2022_03_17_194725_create_votes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateVotesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('votes', function (Blueprint $table) {
             $table->id();
-            $table->unique(['idea_id','user_id']);
+            $table->unique(['idea_id', 'user_id']);
             $table->foreignId('idea_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
@@ -31,4 +31,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('votes');
     }
-};
+}
